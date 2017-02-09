2cd7184204c4277e0caa27985b84bf7f85a628a6
Quoc-Viet Nguyen <afelion@gmail.com>
2017-02-09 22:12:37 +1000

    Restore serial config when closing on Windows (#8)
    Fixes #5

--- syscall_windows.go
+++ syscall_windows.go
@@ -4,5 +4,9 @@ package serial
 
 // Windows api calls
 
+//go:generate go run $GOROOT/src/syscall/mksyscall_windows.go -output zsyscall_windows.go $GOFILE
+
+//sys GetCommState(handle syscall.Handle, dcb *c_DCB) (err error)
 //sys SetCommState(handle syscall.Handle, dcb *c_DCB) (err error)
+//sys GetCommTimeouts(handle syscall.Handle, timeouts *c_COMMTIMEOUTS) (err error)
 //sys SetCommTimeouts(handle syscall.Handle, timeouts *c_COMMTIMEOUTS) (err error)
